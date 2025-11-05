2d48af8bbc77e32759c4f74045df59b1e36369f0
reazndev <reazn.dev@gmail.com>
2025-11-05 17:42:50 +0100

    implement routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { CreateAccountComponent } from './pages/create-account/create-account.component';
+import { NeuanmeldungComponent } from './pages/neuanmeldung/neuanmeldung.component';
+import { SupportComponent } from './pages/support/support.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'create-account', component: CreateAccountComponent },
+  { path: 'registration', component: NeuanmeldungComponent },
+  { path: 'support', component: SupportComponent }
+];
