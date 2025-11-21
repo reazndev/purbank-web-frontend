b6786367fc38fad170d62fc4b85c4bbc9646f782
reazndev <reazn.dev@gmail.com>
2025-11-21 14:58:06 +0100

    add blank dashboard page

---
+++ src/app/pages/dashboard/dashboard.ts
@@ -0,0 +1,13 @@
+import { Component,} from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { LanguageService } from '../../shared/services/language.service';
+
+@Component({
+  selector: 'app-dashboard',
+  imports: [],
+  templateUrl: './dashboard.html',
+  styleUrl: './dashboard.css',
+})
+export class DashboardComponent {
+
+}
