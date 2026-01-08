a5760f52f8e74279ec551bd12de252a636bbfe0f
reazndev <ruby.florian@proton.me>
2026-01-08 22:38:26 +0100

    admin page use 24h format

--- src/app/components/admin/audit-logs/audit-logs.component.ts
+++ src/app/components/admin/audit-logs/audit-logs.component.ts
@@ -80,6 +80,14 @@ export class AuditLogsComponent implements OnInit {
   }
 
   formatDate(dateStr: string): string {
-    return new Date(dateStr).toLocaleString();
+    return new Date(dateStr).toLocaleString('de-CH', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: false
+    });
   }
 }
